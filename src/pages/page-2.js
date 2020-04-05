58e38a96d1da02cc24950400443d1d18205f4708
Mat Johnson <matjohnson@gmail.com>
2020-04-05 11:56:49 -0700

    One more

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -7,7 +7,7 @@ import { Container } from '@components/global';
 const SecondPage = () => (
   <Layout>
     <Container>
-      <h1>Hi from the second page</h1>
+      <h1>Hi from the second page.</h1>
       <p>Welcome to page 2. Also known as the second page.</p>
       <Link to="/">Go back to the homepage</Link>
     </Container>
